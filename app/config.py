0a4cffea4fd78e0e400ac56ea703d8d3bacb15d3
Jon K <jonathankerk@gmail.com>
2020-10-25 22:54:48 +0800

    added back local db configs

--- app/config.py
+++ app/config.py
@@ -6,9 +6,9 @@ class Config(object):
     POSTGRES_URL = "localhost"
     POSTGRES_DB = "fyp-20s4-06p"
     SECRET_KEY = 'secret'
-    # uri_template = 'postgresql+psycopg2://{usr}:{pw}@{url}/{db}'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or uri_template.format(
-    # usr=POSTGRES_USER, pw=POSTGRES_PASSWORD, url=POSTGRES_URL, db=POSTGRES_DB)
-    #
+    uri_template = 'postgresql+psycopg2://{usr}:{pw}@{url}/{db}'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or uri_template.format(
+    usr=POSTGRES_USER, pw=POSTGRES_PASSWORD, url=POSTGRES_URL, db=POSTGRES_DB)
+
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
