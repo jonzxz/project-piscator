8658d3264632a78c736b7b63eb11a922383749b5
Jon K <jonathankerk@gmail.com>
2020-11-16 22:14:58 +0800

    added function to retrieve imap svr

--- app/utils/EmailUtils.py
+++ app/utils/EmailUtils.py
@@ -6,7 +6,7 @@ import imaplib
 def test_mailbox_conn(email_addr, password):
     try:
         logger.info("Connecting to mailbox..")
-        mail_provider = 'imap.gmail.com'
+        mail_provider = get_imap_svr(email_addr)
         mailbox = MailBox(mail_provider)
         mailbox.login(email_addr, password)
         conn_status = True
@@ -16,3 +16,11 @@ def test_mailbox_conn(email_addr, password):
         conn_status = False
         logger.error("Attempting connection to mailbox failed!")
     return conn_status
+
+def get_imap_svr(email_addr):
+    mail_provider = email_addr.split(sep='@')[1]
+    if 'gmail' in mail_provider:
+        return 'imap.gmail.com'
+    if 'hotmail' in mail_provider or 'live' in mail_provider or 'outlook' in mail_provider:
+        return 'outlook.office365.com'
+    # if hotmail / live in mail provider return outlook imap svr
