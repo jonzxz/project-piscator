4c21df59bcd29d7b8fda9d6ae3ace695f6a2331d
Jon K <jonathankerk@gmail.com>
2020-11-21 20:21:31 +0800

    deleted extra files, updated login form

--- app/forms/LoginForm.py
+++ app/forms/LoginForm.py
@@ -4,7 +4,7 @@ from wtforms import FormField, StringField, PasswordField, SubmitField, BooleanF
 from wtforms.validators import DataRequired, ValidationError
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', render_kw={"placeholder": "Username"}, validators=[DataRequired()])
+    password = PasswordField('Password', render_kw={"placeholder": "Password"}, validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
