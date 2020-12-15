be677a51da16d947e68d0ef323512a4c36e1ae09
Karyannis <73423084+Karyannis@users.noreply.github.com>
2020-12-15 14:23:51 +0800

    Update test_admin.py
    Update funcational test for admin login and logout

--- tests/functional/test_admin.py
+++ tests/functional/test_admin.py
@@ -25,11 +25,11 @@ def test_login(driver):
     driver.find_element(By.XPATH, '/html/body/header/div[1]/div/div/div/nav/div[2]/a').click()
     assert driver.current_url.split(sep='/')[-1] == 'login'
     # Enter credentials
-    driver.find_element_by_id('username').send_keys("testuser123")
+    driver.find_element_by_id('username').send_keys("admin")
     driver.find_element_by_id('password').send_keys("password")
     driver.find_element_by_id('submit').click()
-    # Assert user is redirected to dashboard
-    assert driver.current_url.split(sep='/')[-1] == 'dashboard'
+    # Assert user is redirected to admin
+    assert driver.current_url.split(sep='/')[-1] == 'admin'
     sleep(2)
     # Logout
     wait_logout = WebDriverWait(driver, 5)
