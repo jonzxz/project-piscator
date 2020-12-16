5da303b03eb1f6ae58569f0520885075fa0c2bde
Jon K <jonathankerk@gmail.com>
2020-12-16 23:00:41 +0800

    merged admin functional test into dev

--- tests/functional/test_4_admin_authentication.py
+++ tests/functional/test_4_admin_authentication.py
@@ -3,23 +3,8 @@ import sys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
-# Clicks on 'Get Started', enter register credentials and check user is redirected to dashboard
-# Finally logs out
-# def test_register(driver):
-#     # Click 'Get Started'
-#     driver.find_element(By.XPATH, '//*[@id="home"]/div[1]/div[1]/div/div/a').click()
-#     assert driver.current_url.split(sep='/')[-1] == 'register'
-#     # Enter credentials
-#     driver.find_element_by_id('username').send_keys("testuser123")
-#     driver.find_element_by_id('password').send_keys("password")
-#     driver.find_element_by_id('confirm_password').send_keys("password")
-#     driver.find_element_by_id('submit').click()
-#     # Assert user is redirected to dashboard
-#     assert driver.current_url.split(sep='/')[-1] == 'dashboard'
-
-# Clicks on 'Sign In', enter login credentials and check user is redirected to dashboard
+# Clicks on 'Sign In', enter login credentials and check admin is redirected to admin dashboard
 def test_login(driver):
     # Click 'Sign In'
     driver.find_element(By.XPATH, '/html/body/header/div[1]/div/div/div/nav/div[2]/a').click()
@@ -28,10 +13,9 @@ def test_login(driver):
     driver.find_element_by_id('username').send_keys("admin")
     driver.find_element_by_id('password').send_keys("password")
     driver.find_element_by_id('submit').click()
-    # Assert user is redirected to admin
+    # Assert user is redirected to admin dashboard
     # [-2] due to /admin/
     assert driver.current_url.split(sep='/')[-2] == 'admin'
-    sleep(2)
     # Logout
     wait_logout = WebDriverWait(driver, 5)
     wait_logout.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mySidepanel"]/a[5]')))
