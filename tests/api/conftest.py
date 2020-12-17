79fc36d299719746683de14e3d9160dfaf863797
Jon K <jonathankerk@gmail.com>
2020-12-17 22:17:29 +0800

    changed api test scope to function, added vars in test and implemented admin auth api tests

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -2,17 +2,17 @@ import pytest
 from app import app as flask_app
 from app import db as ps_db
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def app():
     yield flask_app
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def client(app):
     # WTF_CSRF_ENABLED = False to allow form submission in tests
     app.config['WTF_CSRF_ENABLED'] = False
     app.config['TESTING'] = True
     return app.test_client()
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def db():
     yield ps_db
---
+++ tests/api/test_4_admin_authentication.py
@@ -0,0 +1,18 @@
+from app.models.User import User
+import pytest
+from test_2_authentication import login
+
+# Assert HTTP code, assert URL redirect
+def test_valid_login(client):
+    USERNAME = 'admin'
+    PASSWORD = 'password'
+    response = login(client, USERNAME, PASSWORD)
+    assert response.status_code == 200
+    assert b'Admin Dashboard' in response.data
+
+def test_invalid_login(client):
+    USERNAME = 'INVALIDADMIN'
+    PASSWORD = 'NOTpassword'
+    response = login(client, USERNAME, PASSWORD)
+    assert response.status_code == 200
+    assert not b'Admin Dashboard' in response.data
