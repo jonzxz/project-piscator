918b405ae4add4ee8c9000ab031e4e054055ec30
Jon K <jonathankerk@gmail.com>
2020-12-25 13:50:55 +0800

    updated route check_phish to add to database for phishing mails detected

--- app/models/PhishingEmail.py
+++ app/models/PhishingEmail.py
@@ -5,12 +5,13 @@ from datetime import datetime
 class PhishingEmail(db.Model):
     __tablename__ = 'phishing_email'
     mail_id = db.Column(db.Integer, primary_key=True)
-    sender_address = db.Column(db.String(30), index=True, unique=True, nullable=False)
+    sender_address = db.Column(db.String(30), index=True, unique=False, nullable=False)
+    subject = db.Column(db.Text, nullable=False, unique=False)
     content = db.Column(db.Text, nullable=False, unique=False)
     created_at = db.Column(db.DateTime, index=True,default=datetime.now)
 
     # FK
-    receiver_address = db.Column(db.Integer, db.ForeignKey('email_address.email_id'), index=True, unique=False, nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('email_address.email_id'), index=True, unique=False, nullable=False)
 
     def __repr__(self):
-        return "Phishing Mail from: {} -- Received by: {}".format(self.sender_address, self.receiver_address)
+        return "Phishing Mail from: {} -- Received by: {}".format(self.sender_address, self.receiver_id)
