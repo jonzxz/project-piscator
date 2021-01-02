c3fb76b04c0cb27dc5f1e150ebbf336e4b0e7d98
Jon K <jonathankerk@gmail.com>
2021-01-02 20:54:26 +0800

    added email field in reset form

--- app/forms/ResetPasswordForm.py
+++ app/forms/ResetPasswordForm.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
 from app.models import User
 from wtforms import FormField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, ValidationError, Email
 
 class ResetPasswordForm(FlaskForm):
     username = StringField('Username', render_kw={"placeholder": "Username"}, validators=[DataRequired()])
+    email_address = EmailField('Email Address', render_kw={"placeholder": "Email Address"} \
+    , validators=[DataRequired(), Email()])
     submit = SubmitField('Reset')
