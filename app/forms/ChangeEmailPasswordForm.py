39cf47bb6840f5da34a61aff6a81be5b8442d517
Karyannis <73423084+Karyannis@users.noreply.github.com>
2021-01-07 20:34:21 +0800

    Forms for change the email password

---
+++ app/forms/ChangeEmailPasswordForm.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import FormField, StringField, PasswordField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
+
+class ChangeEmailPasswordForm(FlaskForm):
+    email_address = EmailField('Email Address', validators=[DataRequired(), Email()])
+    new_password = PasswordField('New Password: ', render_kw={"placeholder": "New Password"})
+    submit = SubmitField('Change')
