36c3e4eda5763d264218171d777ba395ff9521e0
Jon K <jonathankerk@gmail.com>
2021-01-17 20:20:37 +0800

    templates, forms, ml cleanup. removed unused assets

--- app/forms/ChangeEmailPasswordForm.py
+++ app/forms/ChangeEmailPasswordForm.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import FormField, StringField, PasswordField, SubmitField
+from wtforms import PasswordField, SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email
 
+# WTForm for "Change Password" in Subscribers' Dashboard for email address
 class ChangeEmailPasswordForm(FlaskForm):
     email_address = EmailField('Email Address', validators=[DataRequired(), Email()])
     new_password = PasswordField('New Password: ', render_kw={"placeholder": "New Password"})
--- app/forms/RegistrationForm.py
+++ app/forms/RegistrationForm.py
@@ -1,25 +1,21 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from app.models import User
-from wtforms import FormField, StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, EqualTo
 
+# WTForm for Registration page
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', render_kw={"placeholder": "Username"}, validators=[DataRequired()])
-	#email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', render_kw={"placeholder": "Password"}, validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password', render_kw={"placeholder": "Confirm Password"},
-		validators=[DataRequired(),
-					EqualTo('password', message='Password must match!')])
-	agreement = BooleanField('Agreement', validators=[DataRequired(message='You must agree to the terms and policies to register!')])
+	username = StringField('Username', render_kw={"placeholder": "Username"}\
+	, validators=[DataRequired()])
+	password = PasswordField('Password', render_kw={"placeholder": "Password"}\
+	, validators=[DataRequired()])
+
+	confirm_password = PasswordField('Confirm Password'\
+	, render_kw={"placeholder": "Confirm Password"}, validators=[DataRequired()\
+	, EqualTo('password', message='Password must match!')])
+
+	agreement = BooleanField('Agreement', validators=[DataRequired(\
+	message='You must agree to the terms and policies to register!')])
+
 	recaptcha = RecaptchaField()
 	submit = SubmitField('Register')
-
-	# def validate_username(self, username):
-	# 	user = User.query.filter_by(username=username.data).first()
-	# 	if user is not None:
-	# 		raise ValidationError('Username already taken')
-
-	# def validate_email(self, email):
-	# 	user = User.query.filter_by(email=email.data).first()
-	# 	if user is not None:
-	# 		raise ValidationError('Email already taken')
