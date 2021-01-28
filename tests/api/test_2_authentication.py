dc8216e4cab9dd10cd4041a41bd36266eef6669f
Joy <joylaw28@hotmail.com>
2021-01-28 22:02:02 +0800

    updated api/test_2_authentication.py to include additional api tests

--- tests/api/test_2_authentication.py
+++ tests/api/test_2_authentication.py
@@ -2,13 +2,13 @@ from app.models.User import User
 import pytest
 from app.utils.DBUtils import get_user_by_name
 
-def register(client, username, password, confirm_password):
+def register(client, username, password, confirm_password, agreement):
     return client.post(
     '/register', data={
     'username' : username,
     'password' : password,
     'confirm_password' : confirm_password,
-    'agreement' : True
+    'agreement' : agreement
     },
     follow_redirects=True
     )
@@ -26,25 +26,65 @@ def logout(client):
     client.get('/logout', follow_redirects=True)
 
 # Assert HTTP code, assert database entry, assert page redirect
+def test_invalid_register_no_input(client, db):
+    response = register(client, '', '', '', None)
+    assert response.status_code == 200
+    assert b'This field is required.', b'You must agree to the terms and policies to register!' in response.data
+
+def test_invalid_register_uncheck_checkbox(client, db):
+    USERNAME = 'testuser123'
+    PASSWORD = 'password'
+    CONF_PASSWORD = 'password'
+    response = register(client, USERNAME, PASSWORD, CONF_PASSWORD, None)
+    assert response.status_code == 200
+    assert b'You must agree to the terms and policies to register!' in response.data
+
+def test_invalid_register_different_passwords(client, db):
+    USERNAME = 'testuser123'
+    PASSWORD = 'password'
+    CONF_PASSWORD = 'password1'
+    response = register(client, USERNAME, PASSWORD, CONF_PASSWORD, True)
+    assert response.status_code == 200
+    assert b'Password must match!' in response.data
+
 def test_valid_register(client, db):
     USERNAME = 'testuser123'
     PASSWORD = 'password'
     CONF_PASSWORD = 'password'
-    response = register(client, USERNAME, PASSWORD, CONF_PASSWORD)
+    response = register(client, USERNAME, PASSWORD, CONF_PASSWORD, True)
     assert response.status_code == 200
     assert get_user_by_name(USERNAME)
-    assert b'Dashboard' in response.data
+    assert b'dashboard' in response.data
     logout(client)
 
-def test_invalid_register(client, db):
+def test_invalid_register_exist(client, db):
     USERNAME = 'testuser123'
     PASSWORD = 'password'
-    CONF_PASSWORD = 'NOTpassword'
-    response = register(client, USERNAME, PASSWORD, CONF_PASSWORD)
+    CONF_PASSWORD = 'password'
+    response = register(client, USERNAME, PASSWORD, CONF_PASSWORD, True)
     assert response.status_code == 200
-    assert b'Create a New Account' in response.data
+    assert b'Username already taken!' in response.data
 
-# Assert HTTP code, assert URL redirect
+def test_invalid_unregistered_login(client):
+    USERNAME = 'testuser123456'
+    PASSWORD = 'password'
+    response = login(client, USERNAME, PASSWORD)
+    assert response.status_code == 200
+    assert b'Invalid username or password' in response.data
+
+def test_invalid_password_login(client):
+    USERNAME = 'testuser123'
+    PASSWORD = 'password123'
+    response = login(client, USERNAME, PASSWORD)
+    assert response.status_code == 200
+    assert b'Invalid username or password' in response.data
+
+def test_invalid_empty_password_login(client):
+    USERNAME = 'testuser123'
+    response = login(client, USERNAME, '')
+    assert response.status_code == 200
+    assert b'Login' in response.data
+
 def test_valid_login(client):
     USERNAME = 'testuser123'
     PASSWORD = 'password'
@@ -52,13 +92,6 @@ def test_valid_login(client):
     assert response.status_code == 200
     assert b'dashboard' in response.data
     logout(client)
-
-def test_invalid_login(client):
-    USERNAME = 'INVALIDUSER'
-    PASSWORD = 'wrongpassword'
-    response = login(client, USERNAME, PASSWORD)
-    assert response.status_code == 200
-    assert b'Invalid username or password' in response.data
 
 def test_logout(client):
     response = client.get('/logout', follow_redirects=True)
